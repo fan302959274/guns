dd56b7ee775ea8c47d530f2f21f660583ebfeffb
songsong818 <songsong818@d03b18ed-6a1a-4ba0-ab31-1c687d3cad1a>
2018-11-23 13:43:23 +0000

    redis服务器切换

--- guns-rest/src/test/java/com/stylefeng/guns/pay/PayTest.java
+++ guns-rest/src/test/java/com/stylefeng/guns/pay/PayTest.java
@@ -10,13 +10,19 @@ import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
 import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
 import com.baomidou.mybatisplus.generator.config.rules.DbType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.stylefeng.guns.core.util.httpclient.HttpClientUtil;
+import com.stylefeng.guns.core.util.response.CommonResp;
+import com.stylefeng.guns.core.util.response.ResponseCode;
 import com.stylefeng.guns.rest.common.wxpay.Configure;
 import com.stylefeng.guns.rest.common.wxpay.PayUtil;
 import com.stylefeng.guns.rest.common.wxpay.Signature;
 import org.junit.Test;
+import org.springframework.http.ResponseEntity;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 实体生成
@@ -28,13 +34,18 @@ public class PayTest {
 
     @Test
     public void entityGenerator() throws Exception {
-        String notityXml = "<xml><appid><![CDATA[wx7fbf903391250b56]]></appid><bank_type><![CDATA[CFT]]></bank_type><cash_fee><![CDATA[1]]></cash_fee><fee_type><![CDATA[CNY]]></fee_type><is_subscribe><![CDATA[N]]></is_subscribe><mch_id><![CDATA[1517700411]]></mch_id><nonce_str><![CDATA[yaaepvqsii4ui1kigih10tm9mny5a4n5]]></nonce_str><openid><![CDATA[osO_i5Nw8D7tILw9qFq7kDJ7-sIk]]></openid><out_trade_no><![CDATA[iqatx10rqfkk55b82tqvwwt0jph2hxce]]></out_trade_no><result_code><![CDATA[SUCCESS]]></result_code><return_code><![CDATA[SUCCESS]]></return_code><sign><![CDATA[4A9BC1B49C178A45243EC55C24CD5FC2]]></sign><time_end><![CDATA[20181119171844]]></time_end><total_fee>1</total_fee><trade_type><![CDATA[JSAPI]]></trade_type><transaction_id><![CDATA[4200000216201811193015378563]]></transaction_id></xml>";
+//        String notityXml = "<xml><appid><![CDATA[wx7fbf903391250b56]]></appid><bank_type><![CDATA[CFT]]></bank_type><cash_fee><![CDATA[1]]></cash_fee><fee_type><![CDATA[CNY]]></fee_type><is_subscribe><![CDATA[N]]></is_subscribe><mch_id><![CDATA[1517700411]]></mch_id><nonce_str><![CDATA[yaaepvqsii4ui1kigih10tm9mny5a4n5]]></nonce_str><openid><![CDATA[osO_i5Nw8D7tILw9qFq7kDJ7-sIk]]></openid><out_trade_no><![CDATA[iqatx10rqfkk55b82tqvwwt0jph2hxce]]></out_trade_no><result_code><![CDATA[SUCCESS]]></result_code><return_code><![CDATA[SUCCESS]]></return_code><sign><![CDATA[4A9BC1B49C178A45243EC55C24CD5FC2]]></sign><time_end><![CDATA[20181119171844]]></time_end><total_fee>1</total_fee><trade_type><![CDATA[JSAPI]]></trade_type><transaction_id><![CDATA[4200000216201811193015378563]]></transaction_id></xml>";
+//
+//
+//
+//        Map map = PayUtil.doXMLParse(notityXml);
+//        String sign = PayUtil.sign(PayUtil.createLinkString(PayUtil.paraFilter(map)), Configure.getKey(),"utf-8");
+//        boolean b = PayUtil.verify(PayUtil.createLinkString(PayUtil.paraFilter(map)), (String) map.get("sign"), Configure.getKey(), "utf-8");
+//        System.out.println("12312");
 
 
-
-        Map map = PayUtil.doXMLParse(notityXml);
-        String sign = PayUtil.sign(PayUtil.createLinkString(PayUtil.paraFilter(map)), Configure.getKey(),"utf-8");
-        boolean b = PayUtil.verify(PayUtil.createLinkString(PayUtil.paraFilter(map)), (String) map.get("sign"), Configure.getKey(), "utf-8");
-        System.out.println("12312");
+        //发送第三方 TODO2018112321382320659
+        String result = new HttpClientUtil().doPost("http://utf8.api.smschinese.cn/?Uid=毕盛小程序&Key=d57ba7e3dcc263b1ff5f&smsMob=13651638713&smsText=【球王决】赛制为7十1裁判为一主两边，同时我们为您的球队赠送恒大冰泉一箱", new HashMap<>(), "utf-8");
+        System.out.println(result);
     }
 }
